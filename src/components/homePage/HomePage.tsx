1e0fa6e783c4991093b0cd88dea727669e2ee384
Admin <1119321.ab@mail.ru>
2025-02-27 20:09:12 +0100

    2 commit

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,20 +1,12 @@
-
 import styles from './homePage.module.css'
-import HomePageLessons from '../homePageLessons/HomePageLessons';
-import HomePageHomeworks from '../homePageHomeworks/HomePageHomeworks';
-// import HomePageConsultations from "../homePageConsultations/HomePageConsultations";
-import HomePageExercises from '../homePageExercises/HomePageExercises';
-
+import HomePageConsultations from "../homePageConsultations/HomePageConsultations";
 
 export default function HomePage():JSX.Element {
   return (
     <>
       <h2>alexs home page🏠</h2>
       <div className={styles.gridContainer}>
-        <HomePageLessons />
-        <HomePageHomeworks />
-        {/* <HomePageConsultations /> */}
-        <HomePageExercises />
+        <HomePageConsultations />
         {/* <Link to="lesson-01">
           <div>Lesson 1</div>
         </Link> */}
