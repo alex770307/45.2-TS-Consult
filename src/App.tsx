9a7217b52951d653af1db0c852ede582bfbe3fa1
Admin <1119321.ab@mail.ru>
2025-02-27 20:14:26 +0100

    2 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Link, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { FavoritesProvider } from "./favoritesContext/FavoritesContext";
 import { CartProvider } from "./context/CartContext";
 import "./App.css";
